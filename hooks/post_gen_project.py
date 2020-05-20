34ae8a1c0af358a1ff7b7e135c11e0e84263189b
Cory Perkins <link2cory@gmail.com>
2020-05-19 17:28:55 -0700

    added github support

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 import os, subprocess
 
 from cookiecutter.main import cookiecutter
+from github import Github
 from git import Repo
 
 # run cookiecutter for each sub-package
@@ -27,4 +28,16 @@ for name, package in {{ cookiecutter.packages | dictsort }}:
     local_repo = Repo.init(os.getcwd())
     local_repo.git.add(A=True)
     local_repo.index.commit('Initial Commit, project generated with cookiecutter-jam-app')
+
+    {% if cookiecutter.use_github == 'yes' -%}
+        # create the github repo
+        Github('{{ cookiecutter.github_token }}').get_user().create_repo('{{ cookiecutter.project_slug }}')
+        # point local repo to the remote
+        remote_repo = local_repo.create_remote(
+            'origin',
+            'git@github.com:{{ cookiecutter.github_user }}/{{ cookiecutter.project_slug }}.git'
+        )
+        #  push to the remote
+        remote_repo.push(refspec='{}:{}'.format('master', 'master'))
+    {% endif %}
 {% endif %}
