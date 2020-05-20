7a6bc2d9d9f999ce520e6a7c33965e55a53ef3d9
Cory Perkins <link2cory@gmail.com>
2020-05-19 17:28:21 -0700

    added git support

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,7 @@
-import subprocess 
+import os, subprocess
 
 from cookiecutter.main import cookiecutter
+from git import Repo
 
 # run cookiecutter for each sub-package
 for name, package in {{ cookiecutter.packages | dictsort }}:
@@ -18,4 +19,12 @@ for name, package in {{ cookiecutter.packages | dictsort }}:
 # install dependencies
 {% if cookiecutter.install_dependencies == 'yes' -%}
     subprocess.run(['yarn', 'install'])
+{% endif %}
+
+
+{% if cookiecutter.is_subproject == 'no' and cookiecutter.use_git == 'yes' -%}
+    # initialize local repo 
+    local_repo = Repo.init(os.getcwd())
+    local_repo.git.add(A=True)
+    local_repo.index.commit('Initial Commit, project generated with cookiecutter-jam-app')
 {% endif %}
