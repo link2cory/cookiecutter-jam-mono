e8a59cf2c4595ee5ff4626c59c26c243e4e01e2d
Cory Perkins <link2cory@gmail.com>
2020-05-20 15:06:26 -0700

    refactored post_gen_project script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -17,28 +17,27 @@ for name, package in {{ cookiecutter.packages | dictsort }}:
         no_input=True
     )
 
-# Since all sub-packages are assumed to be yarn-based (for now) we want to wait until
-# all sub-packages are setup and then run yarn install from the root package
-{% if cookiecutter.is_subpackage == 'no' and cookiecutter.install_dependencies == 'yes' -%}
-    subprocess.run(['yarn', 'install'])
-{% endif %}
+{% if cookiecutter.is_subpackage == 'no' -%}
+    {% if cookiecutter.install_dependencies == 'yes' -%}
+        subprocess.run(['yarn', 'install'])
+    {% endif %}
 
+    {% if cookiecutter.use_git == 'yes' -%}
+        # initialize local repo 
+        local_repo = Repo.init(os.getcwd())
+        local_repo.git.add(A=True)
+        local_repo.index.commit('Initial Commit, project generated with cookiecutter-jam-app')
 
-{% if cookiecutter.is_subpackage == 'no' and cookiecutter.use_git == 'yes' -%}
-    # initialize local repo 
-    local_repo = Repo.init(os.getcwd())
-    local_repo.git.add(A=True)
-    local_repo.index.commit('Initial Commit, project generated with cookiecutter-jam-app')
-
-    {% if cookiecutter.use_github == 'yes' -%}
-        # create the github repo
-        Github('{{ cookiecutter.github_token }}').get_user().create_repo('{{ cookiecutter.project_slug }}')
-        # point local repo to the remote
-        remote_repo = local_repo.create_remote(
-            'origin',
-            'git@github.com:{{ cookiecutter.github_user }}/{{ cookiecutter.project_slug }}.git'
-        )
-        #  push to the remote
-        remote_repo.push(refspec='{}:{}'.format('master', 'master'))
+        {% if cookiecutter.use_github == 'yes' -%}
+            # create the github repo
+            Github('{{ cookiecutter.github_token }}').get_user().create_repo('{{ cookiecutter.project_slug }}')
+            # point local repo to the remote
+            remote_repo = local_repo.create_remote(
+                'origin',
+                'git@github.com:{{ cookiecutter.github_user }}/{{ cookiecutter.project_slug }}.git'
+            )
+            #  push to the remote
+            remote_repo.push(refspec='{}:{}'.format('master', 'master'))
+        {% endif %}
     {% endif %}
 {% endif %}
