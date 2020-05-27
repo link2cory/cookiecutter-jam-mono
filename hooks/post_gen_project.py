1ba5b4969b9326de39fe05a504ab176741266126
Cory Perkins <link2cory@gmail.com>
2020-05-26 19:43:40 -0700

    refactored hook script

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,6 +6,41 @@ from cookiecutter.main import cookiecutter
 from github import Github
 from git import Repo
 
+def setup_git(): 
+    # activate .gitignore. It must be stored this way to facilitate versioning of
+    # the cookiecutter.
+    os.rename('gitignore', '.gitignore')
+
+    # initialize local repo
+    local_repo = Repo.init(os.getcwd())
+    local_repo.git.add(A=True)
+    local_repo.index.commit('Initial Commit, project generated with cookiecutter-jam-app')
+
+    if context['use_github'] == 'yes':
+        setup_remote(local_repo)
+        add_remote(local_repo)
+
+def setup_remote(local_repo):
+    # create the github repo
+    github_repo = Github('{{ cookiecutter.github_token }}').get_user().create_repo('{{ cookiecutter.project_slug }}')
+
+def add_remote(local_repo):
+    # point local repo to the remote
+    remote_repo = local_repo.create_remote(
+        'origin',
+        'git@github.com:{{ cookiecutter.github_user }}/{{ cookiecutter.project_slug }}.git'
+    )
+
+    #  push to the remote
+    remote_repo.push(refspec='{}:{}'.format('master', 'master'))
+
+def remove_extra_files():
+    # Remove github actions files unless they are necessary
+    if context['use_github'] == 'no' or context['continuous_integration'] != 'github_actions':
+        # remove the github directory
+        os.rmtree('.github/')
+
+
 # extract the context from the cookiecutter jinja2 obj
 # it is much easier to work with this way...
 context = {{ cookiecutter }}
@@ -23,32 +58,10 @@ for name, package in context['packages'].items():
         no_input=True
     )
 
+if context['use_git'] == 'yes':
+    setup_git()
+
 if context['is_subpackage'] == 'no':
     if context['install_dependencies'] == 'yes':
         subprocess.run(['yarn', 'install'])
 
-    if context['use_git'] == 'yes':
-        # activate .gitignore. It must be stored this way to facilitate versioning of
-        # the cookiecutter.
-        os.rename('gitignore', '.gitignore')
-
-        # initialize local repo
-        local_repo = Repo.init(os.getcwd())
-        local_repo.git.add(A=True)
-        local_repo.index.commit('Initial Commit, project generated with cookiecutter-jam-app')
-
-        if context['use_github'] == 'yes':
-            # create the github repo
-            Github('{{ cookiecutter.github_token }}').get_user().create_repo('{{ cookiecutter.project_slug }}')
-            # point local repo to the remote
-            remote_repo = local_repo.create_remote(
-                'origin',
-                'git@github.com:{{ cookiecutter.github_user }}/{{ cookiecutter.project_slug }}.git'
-            )
-            #  push to the remote
-            remote_repo.push(refspec='{}:{}'.format('master', 'master'))
-
-    # Remove github actions files unless they are necessary
-    if context['use_github'] == 'no' || context['continuous_integration'] != 'github_actions':
-        # remove the github directory
-        os.rmtree('.github/')
